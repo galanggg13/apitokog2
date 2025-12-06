fa467e7985d7676431f62a40b4c4265f085d9025
galanggg13 <galangwahyuramadhani@gmail.com>
2025-12-06 12:14:23 +0700

    update migrations & models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -10,17 +10,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
-            $table->string('role')->default('user');
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-
-            // $table->string('phone')->nullable();
-
             $table->string('image')->nullable();
             $table->string('address')->nullable();
-            $table->string('api_token')->nullable();
-
+            $table->string('api_token', 80)->nullable()->unique();
             $table->timestamps();
         });
     }
--- database/migrations/2025_12_05_081652_create_products_table.php
+++ database/migrations/2025_12_05_081652_create_products_table.php
@@ -10,14 +10,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id('product_id');
-            $table->string('image')->nullable();
-            $table->string('colors')->nullable();
             $table->string('title');
-            $table->decimal('price', 10, 2);
             $table->text('description')->nullable();
-            $table->integer('rating')->default(0);
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->decimal('price', 10, 2);
             $table->integer('stock')->default(0);
+            $table->string('image')->nullable();
+            $table->foreignId('category_id')
+                ->constrained('categories', 'category_id')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
